ec557398f7cfc066213d35122ab286f889f10b0f
Arrafi2644 <raffian2644@gmail.com>
2024-12-24 09:28:25 +0600

    Complete get tutors operation

--- index.js
+++ index.js
@@ -5,26 +5,39 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.duk9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
+const tutorCollection = client.db("LearnLang").collection("tutors")
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
+
+    app.get('/tutors', async(req, res)=>{
+        const cursor = tutorCollection.find()
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
